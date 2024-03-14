69ad746548b82c07692eb021655befd7b051ebb5
diskala <143798335+diskala@users.noreply.github.com>
2024-03-14 22:44:00 +0100

    correction

--- config/bundles.php
+++ config/bundles.php
@@ -1,36 +1,20 @@
 <?php
 
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Bundle\DebugBundle\DebugBundle;
-use Symfony\Bundle\MakerBundle\MakerBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
-use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
-use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
-use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
-use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle;
-use SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle;
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-  
 ];
+
